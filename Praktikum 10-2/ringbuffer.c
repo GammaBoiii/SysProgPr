3abdd5fa1e33110d11adb48428eeaebbdedbc493
GammaBoiii <jonny.ludi.jl@gmail.com>
2026-06-08 19:01:30 +0200

    fixes stuff

--- Praktikum 10-2/ringbuffer.c
+++ Praktikum 10-2/ringbuffer.c
@@ -9,8 +9,134 @@
 #include <sys/wait.h>  //wait (für child)
 #include "commonfile.h"
 
+volatile sig_atomic_t timeout_triggered = 0; //diese variable ist signal (asynchron) sicher
+int timeouts = 0;
+
+struct Cleaner myCleaner;
+
+void handleTimeout(int signum) {
+    (void)signum; //warnung bei unused parameter umgehen..
+   // printf("Keine Nachricht für 3 Sekunden empfangen. Tschüss\n");
+    timeout_triggered = 1;
+    //HIER BLOß KEIN EXIT!
+}
+
 int doParent(int shmid, int semid, const char *path); // erzeuger
 void doChild(int shmid, int semid);                    // verbraucher
+
+// hilfsmethode für putsem
+void P(int semid, int semnum) {
+    struct sembuf op = {
+        semnum,
+        -1,
+        0 // SEM_UNDO autom. Freigeben der Semaphoren (man semop) -> schlgt bei großen binärdateien fehl!!!
+     };
+    if(semop(semid, &op, 1) < 0) {
+        perror("semop P");
+        exit(EXIT_FAILURE);
+    }
+}
+
+//hilfsmethode für putsem, aber mit absicherung vor anderen unterbrechungen/signalen
+//erstaz für P() bei dem 3Sekunden Timer
+int P_safe(int semid, int semnum) {
+    struct sembuf op = { semnum, -1, 0 };
+    
+    while (semop(semid, &op, 1) < 0) { //whileschleife startet semop hier neu, wenn es unterbrochen wurde (gibt dann nämlich -1 aus)
+        if (errno == EINTR) {
+            if(timeout_triggered) {
+                timeout_triggered = 0;
+                if(timeouts < 2) {
+                    timeouts++;
+                    printf("\tWarte...\t");
+                    fflush(stdout);
+                    alarm(1);                    
+                    continue;
+                } else {
+                    printf("\n");
+                    return 0; //timer ausgelöst
+                }
+        
+            } else {
+                continue; //kein timer signal, also erstmal weiter warten
+            }
+        }
+        perror("semop P safe");
+        exit(EXIT_FAILURE);
+    }
+    return 1;
+}
+
+//hilfsmethode für getsem
+void V(int semid, int semnum) {
+    struct sembuf op = {
+        semnum,
+        1,
+        0
+    };
+    if(semop(semid, &op, 1) < 0) {
+        perror("semop V");
+        exit(EXIT_FAILURE);
+    }
+}
+
+void setupSems(int semid) {
+    union semun args; 
+    // Semaphore: max Prozesse, die erzeugen dürfen
+    // DEPRICATED
+    /*args.val = 1;
+    if(semctl(semid, 0, SETVAL, args) < 0) {
+        perror("semctl 0");
+        exit(EXIT_FAILURE);
+    }*/ 
+
+    // Semaphorne: freie bytes (256)
+    args.val = MEMSZE;
+    if(semctl(semid, 0, SETVAL, args) < 0) {
+        perror("semctl 1");
+        exit(EXIT_FAILURE);
+    }
+
+    // Semaphore: lesbare bytes
+    args.val = 0;
+    if(semctl(semid, 1, SETVAL, args) < 0) {
+        perror("semctl 2");
+        exit(EXIT_FAILURE);
+    }
+
+    // Semaphore: max Prozesse, die konsumieren dürfen
+    // DEPRICATED
+    /*args.val = 1;
+    if(semctl(semid, 3, SETVAL, args) < 0) {
+        perror("semctl 3");
+        exit(EXIT_FAILURE);
+    } */
+}
+
+
+void cleanup() {
+    printf("Aufräumarbeiten werden durchgeführt...\n");
+    if(shmctl(myCleaner.shmid, IPC_RMID, NULL) < 0) {
+        perror("shmctl IPC_RMID");
+    }
+    if(semctl(myCleaner.semid, 0, IPC_RMID) < 0) {
+        perror("semctl IPC_RMID");
+    }
+}
+
+void cleanExit(int signum) {
+    printf("Zeit zum Aufräumen:\n");
+    printf("Signal %d empfangen, beende...\n", signum);
+    cleanup();
+    printf("Es wurde aufgeräumt, adios!\n");
+    exit(EXIT_SUCCESS);
+}
+
+// DEPRECATED
+void cleanExitChild(int signum) {
+    printf("Signal %d empfangen, beende Kindprozess...\n", signum);
+    timeout_triggered = 1; //lässt Kind denken, dass Timer getriggert ist
+}
 
 int main(int argc, char const *argv[])
 {
@@ -54,7 +180,7 @@ int main(int argc, char const *argv[])
     
     // Semaphorne erstellen
     int semid;
-    if ((semid = semget(key, 4, IPC_CREAT | IPC_EXCL | 0600)) < 0)
+    if ((semid = semget(key, 2, IPC_CREAT | IPC_EXCL | 0600)) < 0)
     {
         printf("Semaphore existiert bereits!\n");
         perror("semget");
@@ -71,6 +197,7 @@ int main(int argc, char const *argv[])
     if ((npid = fork()) < 0)
     {
         perror("fork");
+        cleanup();
         exit(EXIT_FAILURE);
     }
     else if (npid == 0)
@@ -112,14 +239,14 @@ int doParent(int shmid, int semid, const char *path)
     if ((rb = shmat(shmid, NULL, 0)) == (RingBuffer *)-1)
     {
         perror("shmat");
-        exit(EXIT_FAILURE);
+        return(EXIT_FAILURE);
     }
 
     FILE *source;
     if ((source = fopen(path, "rb")) == NULL)
     { // das b ist laut manual eine Absicherung..
         perror("fopen");
-        exit(EXIT_FAILURE);
+        return(EXIT_FAILURE);
     }
 
     int cch;           // current char der gelesend wird
@@ -127,10 +254,10 @@ int doParent(int shmid, int semid, const char *path)
 
     while ((cch = fgetc(source)) != EOF)
     {
-        P(semid, 1); // nach schreibplatz im buffer schaue
+        P(semid, 0); // nach schreibplatz im buffer schaue
         rb->buffer[rb->in_pos] = cch;
         rb->in_pos = (rb->in_pos + 1) % MEMSZE; // ringbuffer, daher modulo
-        V(semid, 2);                            // kündigt neues byte an
+        V(semid, 1);                            // kündigt neues byte an
 
         bytecount++;
         if (bytecount % 100 == 0)
@@ -172,7 +299,7 @@ void doChild(int shmid, int semid)
     { 
         alarm(1);
 
-        if (P_safe(semid, 2) == 0)
+        if (P_safe(semid, 1) == 0)
         {
             break;
         }
@@ -182,7 +309,7 @@ void doChild(int shmid, int semid)
         char cch = rb->buffer[rb->out_pos];
         rb->out_pos = (rb->out_pos + 1) % MEMSZE;
 
-        V(semid, 1);
+        V(semid, 0);
         fputc(cch, target);
 
         bytecount++;
